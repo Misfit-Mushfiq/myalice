89c3ad14e3b0cbe5e0d23d763aabac8135025c67
Mushfiq Chowdhury <mushfiqchowdhury@Mushfiqs-MacBook-Pro.local>
2021-07-25 13:35:49 +0600

    SignUp page added

--- lib/screens/signUpScreen.dart
+++ lib/screens/signUpScreen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:myalice/utils/colors.dart';
+import 'package:myalice/utils/constant_strings.dart';
+import 'package:myalice/utils/routes.dart';
 
 class SignUpScreen extends StatelessWidget {
   @override
@@ -12,7 +16,40 @@ class SignUpScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/launch_icon/signUp.gif"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:
+                    Image.asset("assets/launch_icon/signUp.gif", height: 300),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(SIGNUP_SCREEN_INFO,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, height: 1.5)),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  SIGNUP_SCREEN_BOTTOM_TEXT,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey,
+                      height: 1.5),
+                ),
+              ),
+              SizedBox(height: 12),
+              GestureDetector(
+                child: Text(
+                  'Back to Sign In',
+                  style: TextStyle(
+                      color: AliceColors.ALICE_BLUE,
+                      decoration: TextDecoration.underline),
+                ),
+                onTap: () {
+                  Get.offAllNamed(LOGIN_PAGE);
+                },
+              )
             ],
           )),
     );
--- lib/utils/constant_strings.dart
+++ lib/utils/constant_strings.dart
@@ -2,4 +2,7 @@
 const MY_ALICE = 'myalice';
 const ALICE_LABS = 'Alice Labs';
 const LOGIN_SCREEN_TITLE = "Let's sign you in!";
-
+const SIGNUP_SCREEN_INFO =
+    "For now, you can only sign in and use live chat through this app. You can use our web application to sign up and setup myalice.";
+const SIGNUP_SCREEN_BOTTOM_TEXT =
+    "We are trying our best to bring the smoothest user experience in our mobile app.";
